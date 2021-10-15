e4fefbb372db4e07fdc1eb0c64a63b8e4ea1cab8
tanookibruv <erendon1836@gmail.com>
2021-10-14 21:41:22 -0700

    corrected mongoose connection

--- mood-app/server/server.js
+++ mood-app/server/server.js
@@ -3,8 +3,6 @@ const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schemas');
-const mongoose = require('mongoose');
-require('dotenv').config()
 
 
 const app = express();
